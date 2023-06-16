684a139de019d81f6dccd911697d26f4f06d4f16
alinabodnar1 <malina.bodnar@gmail.com>
2023-06-16 13:59:35 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -17,9 +17,9 @@ export const fetchContacts = createAsyncThunk(
 
 export const addContact = createAsyncThunk(
   "contacts/addContact",
-  async ([name, number],  thunkAPI) => {
+  async (name, number,  thunkAPI) => {
     try {
-      const response = await axios.post("/contacts",  [name, number] );
+      const response = await axios.post("/contacts",  name, number);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
