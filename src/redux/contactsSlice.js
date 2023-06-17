9fab87ceed686829dedef673a39ff24544dbf7a8
alinabodnar1 <malina.bodnar@gmail.com>
2023-06-17 22:31:03 +0300

    change contactsSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -17,38 +17,36 @@ const handleRejected = (state, action) => {
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: contactsInitialState,
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled]:(state, action) => {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [fetchContacts.rejected]: handleRejected,
-
-    [addContact.pending]: handlePending,
-    [addContact.fulfilled]:(state, action) => {
-      state.isLoading = false;
-      state.error = null;
-      // state.items.push(action.payload); 
-      if (state.items.find(contact => contact.name.toLowerCase().trim() === action.payload.name.toLowerCase().trim())) {
-            return alert(`The contact "${action.payload.name}" is already in the contacts!`);
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        if (state.items.find(contact => contact.name.toLowerCase().trim() === action.payload.name.toLowerCase().trim())) {
+            alert(`The contact "${action.payload.name}" is already in the contacts!`);
         } else  if (state.items.find(contact => contact.number.toLowerCase().trim() === action.payload.number.toLowerCase().trim())) {
-            return alert(`The number "${action.payload.number}" is already in the contacts!`);
-        }   else{
-            return state.items.push(action.payload);  
+            alert(`The number "${action.payload.number}" is already in the contacts!`);
+        } else {
+           state.items.push(action.payload); 
         }
-    },
-    [addContact.rejected]: handleRejected,
-
-    [deleteContact.pending]: handlePending,
-    [deleteContact.fulfilled]:(state, action) => {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(contact => contact.id === action.payload);
-      state.items.splice(index, 1);
-    },
-    [deleteContact.rejected]: handleRejected,
+      })
+      .addCase(addContact.rejected, handleRejected)
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(contact => contact.id === action.payload);
+        state.items.splice(index, 1);
+      })
+      .addCase(deleteContact.rejected, handleRejected)
   },
 });
 
