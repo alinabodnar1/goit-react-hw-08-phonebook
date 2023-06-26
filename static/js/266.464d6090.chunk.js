0d9df08fd12a5cff3fc09528323478948cc67116
alinabodnar1 <114241541+alinabodnar1@users.noreply.github.com>
2023-06-26 09:04:34 +0000

    Deploying to gh-pages from @ alinabodnar1/goit-react-hw-08-phonebook@291fcc3a7d91bfc5e0c55fb1fcde58179106bd85 🚀

--- static/js/266.464d6090.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Phonebook welcome page"})})}}}]);
-//# sourceMappingURL=266.464d6090.chunk.js.map
